c47469ab04a4267b22946090f0e3d87b45398c25
jana <janarthan.paramanandam@ge.com>
2018-11-19 20:59:22 +0530

    Add Dog model

---
+++ spring-data/src/main/java/org/jana/springdata/model/Dog.java
@@ -0,0 +1,41 @@
+package org.jana.springdata.model;
+
+public class Dog {
+
+    private String id;
+    private String name;
+    private String breed;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", breed='" + breed + '\'' +
+                '}';
+    }
+}
