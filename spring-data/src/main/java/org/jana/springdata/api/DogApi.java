0eb247bcde4e7d3d71e7c309377f7931cd08578a
jana <janarthan.paramanandam@ge.com>
2018-11-19 20:59:00 +0530

    Add insert api

--- spring-data/src/main/java/org/jana/springdata/api/DogApi.java
+++ spring-data/src/main/java/org/jana/springdata/api/DogApi.java
@@ -1,6 +1,10 @@
 package org.jana.springdata.api;
 
+import org.jana.springdata.model.Dog;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +13,15 @@ public class DogApi {
     @RequestMapping("/")
     public String index() {
         return "Greetings from dog api";
+    }
+
+    @PostMapping("/insert")
+    public ResponseEntity<String> insert(@RequestParam String key, @RequestParam String value) {
+        Dog dog = new Dog();
+        dog.setId(key);
+        dog.setName(value);
+        System.out.println("dog = " + dog);
+        return ResponseEntity.ok("Barked");
     }
 
 }
