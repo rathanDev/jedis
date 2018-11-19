3daeff6f95cbcc1588ee46cc437d2689462bce18
jana <janarthan.paramanandam@ge.com>
2018-11-19 15:54:11 +0530

    Retrieve keys

--- redis/src/main/java/org/jana/App.java
+++ redis/src/main/java/org/jana/App.java
@@ -11,62 +11,33 @@ public class App {
     private static final String host = "localhost";
     private static final int port = 6379;
 
-    private static JedisPool pool;
-
-    public App() {
-        pool = new JedisPool(host, port);
-        System.out.println("Constructed");
-    }
-
-    public void addSets() {
-        String key = "members";
-        String member1 = "Jana";
-        String member2 = "Rathan";
-
-        Jedis jedis = pool.getResource();
-
-        try {
-            //save
-            jedis.sadd(key, member1, member2);
-
-            //retrieve
-            Set<String> smembers = jedis.smembers(key);
-            for(String member: smembers) {
-                System.out.println("member = " + member);
-            }
-        } catch (JedisException ex) {
-            System.out.println("ex = " + ex);
-        } finally {
-            if(jedis != null) {
-                pool.returnResource(jedis);
-            }
-        }
-    }
-
     public static void main(String[] args) {
         System.out.println("Redis");
-
-//        App app = new App();
-//        app.addSets();
 
         Jedis jedis = new Jedis(host, port);
         jedis.connect();
         System.out.println("jedis = " + jedis);
         jedis.auth("pola");
 
-        String key = "members";
+        String memberKey = "members";
         String member1 = "Jana";
         String member2 = "Rathan";
 
         try {
             //save
-            jedis.sadd(key, member1, member2);
+            jedis.sadd(memberKey, member1, member2);
 
             //retrieve
-            Set<String> smembers = jedis.smembers(key);
+            Set<String> smembers = jedis.smembers(memberKey);
             for(String member: smembers) {
                 System.out.println("member = " + member);
             }
+
+            Set<String> keys = jedis.keys("*");
+            for(String key: keys) {
+                System.out.println("key = " + key);
+            }
+
         } catch (JedisException ex) {
             System.out.println("ex = " + ex);
         } finally {
