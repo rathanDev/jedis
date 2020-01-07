23d9d53caa6f99e4bd432a8924771b7df0cffa13
janarthan <jana@email.com>
2020-01-07 19:23:30 +0530

    Do aggregation

---
+++ mongo-jedis/src/main/java/org/jana/jedis/config/SimpleMongoConfig.java
@@ -0,0 +1,21 @@
+package org.jana.jedis.config;
+
+import com.mongodb.MongoClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@Configuration
+public class SimpleMongoConfig {
+
+    @Bean
+    public MongoClient mongo() throws Exception {
+        return new MongoClient("localhost");
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() throws Exception {
+        return new MongoTemplate(mongo(), "test");
+    }
+
+}
--- mongo-jedis/src/main/java/org/jana/jedis/service/StudentService.java
+++ mongo-jedis/src/main/java/org/jana/jedis/service/StudentService.java
@@ -5,6 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+import org.springframework.data.mongodb.core.aggregation.GroupOperation;
+import org.springframework.data.mongodb.core.aggregation.SortOperation;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.stereotype.Service;
 import org.jana.jedis.document.Student;
 import org.jana.jedis.request.SaveStudentRequest;
@@ -14,6 +21,10 @@ import java.util.List;
 import java.util.Optional;
 import java.util.logging.Logger;
 
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.newAggregation;
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.group;
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.sort;
+
 @Service
 public class StudentService {
 
@@ -21,6 +32,9 @@ public class StudentService {
 
     @Autowired
     private StudentRepository studentRepository;
+
+    @Autowired
+    MongoTemplate mongoTemplate;
 
     @Cacheable(value = "mongo_students", unless = "#result == null")
     public List<Student> getAll() {
@@ -60,6 +74,20 @@ public class StudentService {
     public String clearCache() {
         log.info("Clear jedis");
         return "Cache cleared";
+    }
+
+    public void doAggregation() {
+        GroupOperation sumZips = group("state").count().as("zipCount");
+        SortOperation sortByCount = sort(Sort.Direction.ASC, "zipCount");
+        GroupOperation groupFirstAndLast = group().first("_id").as("minZipState")
+                .first("zipCount").as("minZipCount").last("_id").as("maxZipState")
+                .last("zipCount").as("maxZipCount");
+
+        Aggregation aggregation = newAggregation(sumZips, sortByCount, groupFirstAndLast);
+
+        AggregationResults<Document> result = mongoTemplate
+                .aggregate(aggregation, "zips", Document.class);
+        Document document= result.getUniqueMappedResult();
     }
 
 }
